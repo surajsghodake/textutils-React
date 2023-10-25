bd638945ec9506d4e4e30e4a702def2019dee878
surajsghodake <117455759+surajsghodake@users.noreply.github.com>
2023-10-25 17:21:37 +0530

    textutils

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
  
 function App() {
@@ -39,10 +39,11 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
+    <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
     {/* <Switch> */}
     {/* /users --> Component 1
         /users/home --> Component 2 */}
@@ -53,13 +54,12 @@ function App() {
             <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
           </Route>
     </Switch> */}
-    <Routes>
+    {/* <Routes>
       <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}/>
-      {/* <Route path="/pricing" element={<Pricing/>}/> */}
       <Route exact path="/about" element={<About/>}/>
-    </Routes>
+    </Routes> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </> 
   );
 }
